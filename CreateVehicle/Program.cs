8425091aad54cd5a2a51c3a3fe4780f3f4cc4b06
kristena11 <kristenalbrecht@outlook.com>
2018-06-09 17:32:41 -0700

    first commit

---
+++ CreateVehicle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateVehicle
+{
+       
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vehicle[] vehicles = new Vehicle[8];
+            vehicles[0] = new Vehicle("Aston Martin", "Vantage", 129000, 11111111111111111, new DateTime(2016, 1, 27), false);
+            vehicles[1] = new Vehicle("Ford", "fusion", 28678.99, 22222222222222222, new DateTime(2016, 2, 24), false);
+            vehicles[2] = new Vehicle("Honda", "Civic EX", 18713.27, 33333333333333333, new DateTime(2016, 2, 24), false);
+            vehicles[3] = new Vehicle("Volkswagen", "Tiguan", 22250.23, 44444444444444444, new DateTime(2016, 3, 11), false);
+            vehicles[4] = new Vehicle("BMW", "X1", 33900.00, 55555555555555555, new DateTime(2016, 4, 03), false);
+            vehicles[5] = new Vehicle("Subaru", "Legacy", 22195.95, 66666666666666666, new DateTime(2016, 5, 16), false);
+            vehicles[6] = new Vehicle("Nissan", "Juke", 20250.50, 77777777777777777, new DateTime(2016, 6, 23), false);
+            vehicles[7] = new Vehicle();
+                   
+            Console.WriteLine("Inventory 6/23/2016\n");
+            foreach (Vehicle aVehicle in vehicles)
+                Console.WriteLine(aVehicle);
+            Console.WriteLine("TOTAL INVENTORY:" + Vehicle.vehicleCount);
+            Console.WriteLine("__________________________________________");
+
+            vehicles[2].UpdateStatus();
+            vehicles[2].UpdateInventory();
+            Console.WriteLine("\n\n\nHonda Civic is SOLD");
+            Console.WriteLine("__________________________________________");
+
+            Console.WriteLine("\n\n\nUpdated Report on Inventory 6/23/2016\n");
+            foreach (Vehicle aVehicle in vehicles)
+                Console.WriteLine(aVehicle);
+            Console.WriteLine("TOTAL INVENTORY:" + Vehicle.vehicleCount);
+            Console.WriteLine("__________________________________________");
+
+            Console.WriteLine("Press any Key to continue....");
+            Console.ReadKey();
+        }
+      
+
+    }
+}
---
+++ CreateVehicle/Vehicle.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateVehicle
+{
+    public class Vehicle
+    {
+
+        #region Fields
+        private long vin;
+        private DateTime DeliveryDate;
+        private bool SoldStatus;
+        private double mrsp;
+        public static int vehicleCount = 0;
+        
+        
+        #endregion End of Fields
+
+        #region Constructors 
+        public Vehicle()
+        {
+            Company = "unknown";
+            Model = "unknown";
+            mrsp = 0;
+            vin = 0;
+            
+            SoldStatus = false;
+            vehicleCount++;
+        }
+        public Vehicle(string company, string model, double mrsp, long vin, DateTime deliveryDate, Boolean soldStatus)
+        {
+            Company = company;
+            Model = model;
+            this.mrsp = mrsp;
+            this.vin = vin;
+            DeliveryDate = deliveryDate;
+            SoldStatus = soldStatus;
+            vehicleCount++;
+        }
+        #endregion End of Constructors
+
+        #region Properties
+        
+        public string Model { get; set; }
+        public string Company { get; set; }
+        public int VehicleCount
+        {
+            get
+            {
+                return vehicleCount;
+            }
+        }
+        public long Vin
+        {
+            get
+            {
+                return vin;
+            }
+            set
+            {
+                if (value > 0)
+                    vin = value;
+                else
+                    vin = 0;
+            }
+        }
+        public double Mrsp
+        {
+            get
+            {
+                return mrsp;
+            }
+            set
+            {
+                if (value > 0)
+                    mrsp = value;
+                else
+                    mrsp = 0;
+            }
+        }
+        public DateTime deliveryDate { get; set; }
+
+        public Boolean soldStatus { get; set; }
+
+
+
+        #endregion End Properties
+
+        #region Methods
+
+        public bool UpdateStatus() => soldStatus = true;
+        public int UpdateInventory() => vehicleCount = vehicleCount - 1;
+
+        public override string ToString()
+        {
+            return $"{Company} {Model}\n MRSP:${Mrsp}\n Vin:{Vin}\n Delivered:{DeliveryDate}\n Sold: {soldStatus}\n";
+        }
+        #endregion End of Methods
+
+    }
+}
